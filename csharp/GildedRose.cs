60c9bc942348c8eaa414a44f87dd995c77be0f57
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-14 10:52:15 +0100

    Refactored code

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework.Internal.Execution;
 
 namespace csharp
 {
@@ -12,78 +14,59 @@ namespace csharp
 
         public void UpdateQuality()
         {
+            List<string> increasingItems = new List<string> {"Aged Brie", "Backstage passes to a TAFKAL80ETC concert"};
+            List<string> legendaryItems = new List<string> {"Sulfuras, Hand of Ragnaros"};
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (legendaryItems.Contains(Items[i].Name))
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    Items[i].SellIn = 0;
+                    continue;
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (increasingItems.Contains(Items[i].Name))
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    Items[i] = IncreasingItem(Items[i]);
+                    continue;
                 }
 
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+                int qualityLoss = -1;
+                if (Items[i].SellIn <= 0)
+                    qualityLoss *= 2;
+                if (Items[i].Name.Contains("Conjured"))
+                    qualityLoss *= 2;
+                Items[i].Quality = Math.Max(0, Items[i].Quality + qualityLoss);
+                Items[i].SellIn -= 1;
             }
+        }
+
+        private Item IncreasingItem(Item inputItem)
+        {
+            int newQuality = inputItem.Quality + 1;
+            if (inputItem.Name == "Aged Brie")
+            {
+                if (inputItem.SellIn <= 0)
+                    newQuality = Math.Min(50, newQuality+1);
+                inputItem.Quality = newQuality;
+            }
+
+            if (inputItem.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (inputItem.SellIn <= 0)
+                {
+                    newQuality = 0;
+                }
+                else if (inputItem.SellIn <= 10)
+                {
+                    newQuality = Math.Min(newQuality+1,50);
+                    if (inputItem.SellIn <= 5)
+                        newQuality = Math.Min(newQuality+1,50);
+                }
+
+                inputItem.Quality = newQuality;
+            }
+            inputItem.SellIn--;
+            return inputItem;
         }
     }
 }
--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -9,10 +9,19 @@ namespace csharp
         [Test]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item>
+            {
+                new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 11, Quality = 5},
+                new Item { Name = "Normal Iron Sword", SellIn = 15, Quality = 25},
+                new Item { Name = "Conjured Iron Sword", SellIn = 15, Quality = 25}
+            };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual(2, Items[0].Quality);
+            app.UpdateQuality();
+            Assert.True(2*(25-Items[2].Quality) == (25-Items[3].Quality));
+            Assert.AreEqual(8, Items[1].Quality);
         }
     }
 }
